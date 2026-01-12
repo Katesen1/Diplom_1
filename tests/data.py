d0770d6eb5ba4fe3aceea772f3f498774921d804
Ekaterina Senko <eka.senko@yandex.ru>
2026-01-12 21:37:09 +0300

    comments corrected#1

--- tests/data.py
+++ tests/data.py
@@ -1,5 +1,8 @@
 from extra.ingredient import Ingredient
 import extra.ingredient_types as ing_type
 
-ing1 = Ingredient(ing_type.INGREDIENT_TYPE_FILLING, "dinosaur", 200)
-ing2 = Ingredient(ing_type.INGREDIENT_TYPE_SAUCE, "sour cream", 200)
+ing_dino = Ingredient(ing_type.INGREDIENT_TYPE_FILLING, "dinosaur", 200)
+ing_cream = Ingredient(ing_type.INGREDIENT_TYPE_SAUCE, "sour cream", 200)
+
+bun_red = ('red bun', 300)
+bun_black = ('black bun', 100)
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -9,37 +9,36 @@ class TestBurger:
         assert burger.bun is None and burger.ingredients == []
 
     def test_set_buns(self, burger: Burger):
-        bun = Bun("red bun", 300)
+        bun = Bun(*data.bun_red)
         burger.set_buns(bun)
         assert burger.bun == bun
 
     def test_add_ingredients(self, burger: Burger):
-        ingredient = data.ing1
+        ingredient = data.ing_dino
         burger.add_ingredient(ingredient)
         assert ingredient in burger.ingredients
 
     @pytest.mark.parametrize(
         "ingredients, index, expected_ingredients",
-        [[[data.ing1], 0, []], [[data.ing1, data.ing2], 0, [data.ing2]], [[data.ing1, data.ing2], 1, [data.ing1]]],
-    )
+        [[[data.ing_dino], 0, []], [[data.ing_dino, data.ing_cream], 0, [data.ing_cream]], [[data.ing_dino, data.ing_cream], 1, [data.ing_dino]]])
     def test_remove_ingredient(self, burger: Burger, ingredients, index, expected_ingredients):
         burger.ingredients = ingredients
         burger.remove_ingredient(index)
         assert burger.ingredients == expected_ingredients
 
     @pytest.mark.parametrize('ingredients, index',
-                             [[[], 0], [[data.ing1], 1]])
+                             [[[], 0], [[data.ing_dino], 1]])
     def test_remove_ingredient_error(self, burger: Burger, ingredients, index):
         burger.ingredients = ingredients
         with pytest.raises(IndexError):
             burger.remove_ingredient(index)
 
     def test_move_ingredients(self, burger: Burger):
-        burger.ingredients = [data.ing1, data.ing2]
+        burger.ingredients = [data.ing_dino, data.ing_cream]
         index = 0
         new_index = 1
         burger.move_ingredient(index, new_index)
-        assert burger.ingredients[0] == data.ing2 and burger.ingredients[1] == data.ing1
+        assert burger.ingredients[0] == data.ing_cream and burger.ingredients[1] == data.ing_dino
 
     def test_move_zero_ingredients(self, burger: Burger):
         burger.ingredients = []
@@ -49,20 +48,20 @@ class TestBurger:
             burger.move_ingredient(index, new_index)
         
     def test_move_more_than_ingredients(self, burger: Burger):
-        burger.ingredients = [data.ing1, data.ing2]
+        burger.ingredients = [data.ing_dino, data.ing_cream]
         index = 2
         new_index = 1
         with pytest.raises(IndexError):
             burger.move_ingredient(index, new_index)
 
     def test_get_price(self, burger: Burger):
-        burger.bun = Bun("black bun", 100)
-        burger.ingredients = [data.ing1, data.ing2]
+        burger.bun = Bun(*data.bun_black)
+        burger.ingredients = [data.ing_dino, data.ing_cream]
         price = burger.get_price()
         assert price == 600
         
     def test_get_receipt(self, burger: Burger):
-        burger.bun = Bun("black bun", 100)
-        burger.ingredients = [data.ing1, data.ing2]
+        burger.bun = Bun(*data.bun_black)
+        burger.ingredients = [data.ing_dino, data.ing_cream]
         receipt = burger.get_receipt()
-        assert '(==== black bun ====)\n= filling dinosaur =\n= sauce sour cream =\n(==== black bun ====)\n\nPrice: 600' in receipt
+        assert '(==== black bun ====)\n= filling dinosaur =\n= sauce sour cream =\n(==== black bun ====)\n\nPrice: 600' == receipt
